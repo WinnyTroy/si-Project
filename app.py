2cd59d2a2fc0ea4c664de1768c2f2dcc3c3a2492
WinnyTroy <kiraguwinnie@gmail.com>
2016-08-24 14:58:00 +0300

    cookies set and prints it out on the screen, so user doesnt have to keep re-entering data

--- app.py
+++ app.py
@@ -5,15 +5,31 @@ from flask import Flask, render_template, url_for, redirect, make_response, requ
 
 app = Flask(__name__)
 
+# json dumps creats json string which is then taken by loads and turned into python code again.
+def get_saved_data():
+    try:
+        data = json.loads(request.cookies.get('cookname'))
+    except TypeError:
+        data = {}
+    return data
+
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = get_saved_data()
+    return render_template('index.html', saves = data)
 
+
+
+# dict translates key value pairs retrieved from calling .items(), which is then bumped into the jspn by .dumps.
+# updates saved data and then sends it back to the user
 @app.route('/save', methods = ['POST'])
 def save():
     response = make_response(redirect(url_for('index')))
-    response.set_cookie('cookname', json.dumps(dict(request.form.items())))
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+    response.set_cookie('cookname', json.dumps(data))
     return response
 
 
